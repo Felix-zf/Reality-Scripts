f5663420a510709f67756d551c5339c4a78c878a
Felix <65265788+Felix-zf@users.noreply.github.com>
2024-12-11 15:53:53 +0800

    Update reality.sh

--- reality.sh
+++ reality.sh
@@ -66,20 +66,24 @@ install_singbox(){
     fi
 
     if [[ $SYSTEM == "Debian" ]]; then
-        sudo curl -fsSL https://sing-box.app/gpg.key -o /etc/apt/keyrings/sagernet.asc
-        sudo chmod a+r /etc/apt/keyrings/sagernet.asc
-        echo "deb [arch=`dpkg --print-architecture` signed-by=/etc/apt/keyrings/sagernet.asc] https://deb.sagernet.org/ * *" | \
-        sudo tee /etc/apt/sources.list.d/sagernet.list > /dev/null
-        sudo apt-get update
-        sudo apt-get install sing-box # or sing-box-beta
-    else
         wget https://github.com/SagerNet/sing-box/releases/download/v"$last_version"/sing-box_"$last_version"_linux_$(archAffix).deb -O sing-box.deb
         dpkg -i sing-box.deb
         rm -f sing-box.deb
+    else
+        wget https://github.com/SagerNet/sing-box/releases/download/v"$last_version"/sing-box_"$last_version"_linux_$(archAffix).rpm -O sing-box.rpm
+        rpm -ivh sing-box.rpm
+        rm -f sing-box.rpm
     fi
 
     rm -f /etc/sing-box/config.json
 
+    if [[ -f "/etc/systemd/system/sing-box.service" ]]; then
+        green "Sing-box 安装成功！"
+    else
+        red "Sing-box 安装失败！"
+        exit 1
+    fi
+    
     # 询问用户有关 Reality 端口、UUID 和回落域名
     read -p "设置 Sing-box 端口 [1-65535]（回车则随机分配端口）：" port
     [[ -z $port ]] && port=$(shuf -i 2000-65535 -n 1)
