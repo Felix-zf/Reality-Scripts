ed1e65c0253c1f4f34a253eff4f8693f64f02657
Felix <65265788+Felix-zf@users.noreply.github.com>
2024-12-12 19:05:02 +0800

    Update reality.sh

--- reality.sh
+++ reality.sh
@@ -1,245 +1,80 @@
-    #!/bin/bash
-    export LANG=en_US.UTF-8
-    red='\033[0;31m'
-    green='\033[0;32m'
-    yellow='\033[0;33m'
-    blue='\033[0;36m'
-    bblue='\033[0;34m'
-    plain='\033[0m'
-    red(){ echo -e "\033[31m\033[01m$1\033[0m";}
-    green(){ echo -e "\033[32m\033[01m$1\033[0m";}
-    yellow(){ echo -e "\033[33m\033[01m$1\033[0m";}
-    blue(){ echo -e "\033[36m\033[01m$1\033[0m";}
-    white(){ echo -e "\033[37m\033[01m$1\033[0m";}
-    readp(){ read -p "$(yellow "$1")" $2;}
-    [[ $EUID -ne 0 ]] && yellow "请以root模式运行脚本" && exit
-    #[[ -e /etc/hosts ]] && grep -qE '^ *172.65.251.78 gitlab.com' /etc/hosts || echo -e '\n172.65.251.78 gitlab.com' >> /etc/hosts
-    if [[ -f /etc/redhat-release ]]; then
-    release="Centos"
-    elif cat /etc/issue | grep -q -E -i "alpine"; then
-    release="alpine"
-    elif cat /etc/issue | grep -q -E -i "debian"; then
-    release="Debian"
-    elif cat /etc/issue | grep -q -E -i "ubuntu"; then
-    release="Ubuntu"
-    elif cat /etc/issue | grep -q -E -i "centos|red hat|redhat"; then
-    release="Centos"
-    elif cat /proc/version | grep -q -E -i "debian"; then
-    release="Debian"
-    elif cat /proc/version | grep -q -E -i "ubuntu"; then
-    release="Ubuntu"
-    elif cat /proc/version | grep -q -E -i "centos|red hat|redhat"; then
-    release="Centos"
-    else 
-    red "脚本不支持当前的系统，请选择使用Ubuntu,Debian,Centos系统。" && exit
+#!/bin/bash
+
+RED="\033[31m"
+GREEN="\033[32m"
+YELLOW="\033[33m"
+PLAIN='\033[0m'
+
+red() {
+    echo -e "\033[31m\033[01m$1\033[0m"
+}
+
+green() {
+    echo -e "\033[32m\033[01m$1\033[0m"
+}
+
+yellow() {
+    echo -e "\033[33m\033[01m$1\033[0m"
+}
+
+REGEX=("debian" "ubuntu" "centos|red hat|kernel|oracle linux|alma|rocky" "'amazon linux'" "fedora" "alpine")
+RELEASE=("Debian" "Ubuntu" "CentOS" "CentOS" "Fedora" "Alpine")
+PACKAGE_UPDATE=("apt-get update" "apt-get update" "yum -y update" "yum -y update" "yum -y update" "apk update -f")
+PACKAGE_INSTALL=("apt -y install" "apt -y install" "yum -y install" "yum -y install" "yum -y install" "apk add -f")
+PACKAGE_UNINSTALL=("apt -y autoremove" "apt -y autoremove" "yum -y autoremove" "yum -y autoremove" "yum -y autoremove" "apk del -f")
+
+[[ $EUID -ne 0 ]] && red "注意：请在root用户下运行脚本" && exit 1
+
+CMD=("$(grep -i pretty_name /etc/os-release 2>/dev/null | cut -d \" -f2)" "$(hostnamectl 2>/dev/null | grep -i system | cut -d : -f2)" "$(lsb_release -sd 2>/dev/null)" "$(grep -i description /etc/lsb-release 2>/dev/null | cut -d \" -f2)" "$(grep . /etc/redhat-release 2>/dev/null)" "$(grep . /etc/issue 2>/dev/null | cut -d \\ -f1 | sed '/^[ ]*$/d')")
+
+for i in "${CMD[@]}"; do
+    SYS="$i" && [[ -n $SYS ]] && break
+done
+
+for ((int = 0; int < ${#REGEX[@]}; int++)); do
+    if [[ $(echo "$SYS" | tr '[:upper:]' '[:lower:]') =~ ${REGEX[int]} ]]; then
+        SYSTEM="${RELEASE[int]}" && [[ -n $SYSTEM ]] && break
     fi
-    vsid=$(grep -i version_id /etc/os-release | cut -d \" -f2 | cut -d . -f1)
-    op=$(cat /etc/redhat-release 2>/dev/null || cat /etc/os-release 2>/dev/null | grep -i pretty_name | cut -d \" -f2)
-    #if [[ $(echo "$op" | grep -i -E "arch|alpine") ]]; then
-    if [[ $(echo "$op" | grep -i -E "arch") ]]; then
-    red "脚本不支持当前的 $op 系统，请选择使用Ubuntu,Debian,Centos系统。" && exit
-    fi
-    version=$(uname -r | cut -d "-" -f1)
-    [[ -z $(systemd-detect-virt 2>/dev/null) ]] && vi=$(virt-what 2>/dev/null) || vi=$(systemd-detect-virt 2>/dev/null)
-    case $(uname -m) in
-    armv7l) cpu=armv7;;
-    aarch64) cpu=arm64;;
-    x86_64) cpu=amd64;;
-    *) red "目前脚本不支持$(uname -m)架构" && exit;;
+done
+
+[[ -z $SYSTEM ]] && red "不支持当前VPS系统, 请使用主流的操作系统" && exit 1
+
+# 检测 VPS 处理器架构
+archAffix() {
+    case "$(uname -m)" in
+        x86_64 | amd64) echo 'amd64' ;;
+        armv8 | arm64 | aarch64) echo 'arm64' ;;
+        s390x) echo 's390x' ;;
+        *) red "不支持的CPU架构!" && exit 1 ;;
     esac
-    #bit=$(uname -m)
-    #if [[ $bit = "aarch64" ]]; then
-    #cpu="arm64"
-    #elif [[ $bit = "x86_64" ]]; then
-    #amdv=$(cat /proc/cpuinfo | grep flags | head -n 1 | cut -d: -f2)
-    #[[ $amdv == *avx2* && $amdv == *f16c* ]] && cpu="amd64v3" || cpu="amd64"
-    #else
-    #red "目前脚本不支持 $bit 架构" && exit
-    #fi
-    if [[ -n $(sysctl net.ipv4.tcp_congestion_control 2>/dev/null | awk -F ' ' '{print $3}') ]]; then
-    bbr=`sysctl net.ipv4.tcp_congestion_control | awk -F ' ' '{print $3}'`
-    elif [[ -n $(ping 10.0.0.2 -c 2 | grep ttl) ]]; then
-    bbr="Openvz版bbr-plus"
+}
+
+install_base(){
+    if [[ ! $SYSTEM == "CentOS" ]]; then
+        ${PACKAGE_UPDATE[int]}
+    fi
+    ${PACKAGE_INSTALL[int]} curl wget sudo tar openssl
+}
+
+install_singbox(){
+    install_base
+
+    last_version=$(curl -s https://data.jsdelivr.com/v1/package/gh/SagerNet/sing-box | sed -n 4p | tr -d ',"' | awk '{print $1}')
+    if [[ -z $last_version ]]; then
+        red "获取版本信息失败，请检查VPS的网络状态！"
+        exit 1
+    fi
+
+    if [[ $SYSTEM == "CentOS" ]]; then
+        wget https://github.com/SagerNet/sing-box/releases/download/v"$last_version"/sing-box_"$last_version"_linux_$(archAffix).rpm -O sing-box.rpm
+        rpm -ivh sing-box.rpm
+        rm -f sing-box.rpm
     else
-    bbr="Openvz/Lxc"
+        wget https://github.com/SagerNet/sing-box/releases/download/v"$last_version"/sing-box_"$last_version"_linux_$(archAffix).deb -O sing-box.deb
+        dpkg -i sing-box.deb
+        rm -f sing-box.deb
     fi
-    hostname=$(hostname)
-    
-    if [ ! -f sbyg_update ]; then
-    green "首次安装Sing-box-yg脚本必要的依赖……"
-    if [[ x"${release}" == x"alpine" ]]; then
-    apk update
-    apk add wget curl tar jq tzdata openssl expect git socat iproute2 iptables
-    apk add virt-what
-    apk add qrencode
-    else
-    if [[ $release = Centos && ${vsid} =~ 8 ]]; then
-    cd /etc/yum.repos.d/ && mkdir backup && mv *repo backup/ 
-    curl -o /etc/yum.repos.d/CentOS-Base.repo http://mirrors.aliyun.com/repo/Centos-8.repo
-    sed -i -e "s|mirrors.cloud.aliyuncs.com|mirrors.aliyun.com|g " /etc/yum.repos.d/CentOS-*
-    sed -i -e "s|releasever|releasever-stream|g" /etc/yum.repos.d/CentOS-*
-    yum clean all && yum makecache
-    cd
-    fi
-    if [ -x "$(command -v apt-get)" ]; then
-    apt update -y
-    apt install jq iptables-persistent -y
-    elif [ -x "$(command -v yum)" ]; then
-    yum update -y && yum install epel-release -y
-    yum install jq -y
-    elif [ -x "$(command -v dnf)" ]; then
-    dnf update -y
-    dnf install jq -y
-    fi
-    if [ -x "$(command -v yum)" ] || [ -x "$(command -v dnf)" ]; then
-    if [ -x "$(command -v yum)" ]; then
-    yum install -y cronie iptables-services
-    elif [ -x "$(command -v dnf)" ]; then
-    dnf install -y cronie iptables-services
-    fi
-    systemctl enable iptables >/dev/null 2>&1
-    systemctl start iptables >/dev/null 2>&1
-    fi
-    if [[ -z $vi ]]; then
-    apt install iputils-ping iproute2 systemctl -y
-    fi
-    
-    packages=("curl" "openssl" "iptables" "tar" "expect" "wget" "xxd" "python3" "qrencode" "git")
-    inspackages=("curl" "openssl" "iptables" "tar" "expect" "wget" "xxd" "python3" "qrencode" "git")
-    for i in "${!packages[@]}"; do
-    package="${packages[$i]}"
-    inspackage="${inspackages[$i]}"
-    if ! command -v "$package" &> /dev/null; then
-    if [ -x "$(command -v apt-get)" ]; then
-    apt-get install -y "$inspackage"
-    elif [ -x "$(command -v yum)" ]; then
-    yum install -y "$inspackage"
-    elif [ -x "$(command -v dnf)" ]; then
-    dnf install -y "$inspackage"
-    fi
-    fi
-    done
-    fi
-    touch sbyg_update
-    fi
-    
-    if [[ $vi = openvz ]]; then
-    TUN=$(cat /dev/net/tun 2>&1)
-    if [[ ! $TUN =~ 'in bad state' ]] && [[ ! $TUN =~ '处于错误状态' ]] && [[ ! $TUN =~ 'Die Dateizugriffsnummer ist in schlechter Verfassung' ]]; then 
-    red "检测到未开启TUN，现尝试添加TUN支持" && sleep 4
-    cd /dev && mkdir net && mknod net/tun c 10 200 && chmod 0666 net/tun
-    TUN=$(cat /dev/net/tun 2>&1)
-    if [[ ! $TUN =~ 'in bad state' ]] && [[ ! $TUN =~ '处于错误状态' ]] && [[ ! $TUN =~ 'Die Dateizugriffsnummer ist in schlechter Verfassung' ]]; then 
-    green "添加TUN支持失败，建议与VPS厂商沟通或后台设置开启" && exit
-    else
-    echo '#!/bin/bash' > /root/tun.sh && echo 'cd /dev && mkdir net && mknod net/tun c 10 200 && chmod 0666 net/tun' >> /root/tun.sh && chmod +x /root/tun.sh
-    grep -qE "^ *@reboot root bash /root/tun.sh >/dev/null 2>&1" /etc/crontab || echo "@reboot root bash /root/tun.sh >/dev/null 2>&1" >> /etc/crontab
-    green "TUN守护功能已启动"
-    fi
-    fi
-    fi
-    
-    v4v6(){
-    v4=$(curl -s4m5 icanhazip.com -k)
-    v6=$(curl -s6m5 icanhazip.com -k)
-    }
-    
-    warpcheck(){
-    wgcfv6=$(curl -s6m5 https://www.cloudflare.com/cdn-cgi/trace -k | grep warp | cut -d= -f2)
-    wgcfv4=$(curl -s4m5 https://www.cloudflare.com/cdn-cgi/trace -k | grep warp | cut -d= -f2)
-    }
-    
-    v6(){
-    v4orv6(){
-    if [ -z $(curl -s4m5 icanhazip.com -k) ]; then
-    echo
-    red "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
-    yellow "检测到 纯IPV6 VPS，添加DNS64"
-    echo -e "nameserver 2a00:1098:2b::1\nnameserver 2a00:1098:2c::1\nnameserver 2a01:4f8:c2c:123f::1" > /etc/resolv.conf
-    endip=2606:4700:d0::a29f:c101
-    ipv=prefer_ipv6
-    else
-    endip=162.159.192.1
-    ipv=prefer_ipv4
-    #echo '4' > /etc/s-box/i
-    fi
-    }
-    warpcheck
-    if [[ ! $wgcfv4 =~ on|plus && ! $wgcfv6 =~ on|plus ]]; then
-    v4orv6
-    else
-    systemctl stop wg-quick@wgcf >/dev/null 2>&1
-    kill -15 $(pgrep warp-go) >/dev/null 2>&1 && sleep 2
-    v4orv6
-    systemctl start wg-quick@wgcf >/dev/null 2>&1
-    systemctl restart warp-go >/dev/null 2>&1
-    systemctl enable warp-go >/dev/null 2>&1
-    systemctl start warp-go >/dev/null 2>&1
-    fi
-    }
-    
-    argopid(){
-    ym=$(cat /etc/s-box/sbargoympid.log 2>/dev/null)
-    ls=$(cat /etc/s-box/sbargopid.log 2>/dev/null)
-    }
-    
-    close(){
-    systemctl stop firewalld.service >/dev/null 2>&1
-    systemctl disable firewalld.service >/dev/null 2>&1
-    setenforce 0 >/dev/null 2>&1
-    ufw disable >/dev/null 2>&1
-    iptables -P INPUT ACCEPT >/dev/null 2>&1
-    iptables -P FORWARD ACCEPT >/dev/null 2>&1
-    iptables -P OUTPUT ACCEPT >/dev/null 2>&1
-    iptables -t mangle -F >/dev/null 2>&1
-    iptables -F >/dev/null 2>&1
-    iptables -X >/dev/null 2>&1
-    netfilter-persistent save >/dev/null 2>&1
-    if [[ -n $(apachectl -v 2>/dev/null) ]]; then
-    systemctl stop httpd.service >/dev/null 2>&1
-    systemctl disable httpd.service >/dev/null 2>&1
-    service apache2 stop >/dev/null 2>&1
-    systemctl disable apache2 >/dev/null 2>&1
-    fi
-    sleep 1
-    green "执行开放端口，关闭防火墙完毕"
-    }
-    
-    openyn(){
-    red "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
-    readp "是否开放端口，关闭防火墙？\n1、是，执行 (回车默认)\n2、否，跳过！自行处理\n请选择【1-2】：" action
-    if [[ -z $action ]] || [[ "$action" = "1" ]]; then
-    close
-    elif [[ "$action" = "2" ]]; then
-    echo
-    else
-    red "输入错误,请重新选择" && openyn
-    fi
-    }
-    
-    install_singbox(){
-    red "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
-    green "一、开始下载并安装Sing-box正式版1.10系列内核……请稍等"
-    echo
-    sbcore=$(curl -Ls https://data.jsdelivr.com/v1/package/gh/SagerNet/sing-box | grep -Eo '"1\.10[0-9\.]*",'  | sed -n 1p | tr -d '",')
-    sbname="sing-box-$sbcore-linux-$cpu"
-    curl -L -o /etc/s-box/sing-box.tar.gz  -# --retry 2 https://github.com/SagerNet/sing-box/releases/download/v$sbcore/$sbname.tar.gz
-    if [[ -f '/etc/s-box/sing-box.tar.gz' ]]; then
-    tar xzf /etc/s-box/sing-box.tar.gz -C /etc/s-box
-    mv /etc/s-box/$sbname/sing-box /etc/s-box
-    rm -rf /etc/s-box/{sing-box.tar.gz,$sbname}
-    if [[ -f '/etc/s-box/sing-box' ]]; then
-    chown root:root /etc/s-box/sing-box
-    chmod +x /etc/s-box/sing-box
-    blue "成功安装 Sing-box 内核版本：$(/etc/s-box/sing-box version | awk '/version/{print $NF}')"
-    else
-    red "下载 Sing-box 内核不完整，安装失败，请再运行安装一次" && exit
-    fi
-    else
-    red "下载 Sing-box 内核失败，请再运行安装一次，并检测VPS的网络是否可以访问Github" && exit
-    fi
-    }
-    
+
     # 询问用户有关 Reality 端口、UUID 和回落域名
     read -p "设置 Sing-box 端口 [1-65535]（回车则随机分配端口）：" port
     [[ -z $port ]] && port=$(shuf -i 2000-65535 -n 1)
